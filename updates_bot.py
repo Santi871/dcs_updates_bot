245ed84da8eee068c94ae94c424e22cdb00cd832
Santi871 <santivegega@gmail.com>
2016-04-08 17:45:45 -0300

    added watch_messages

--- updates_bot.py
+++ updates_bot.py
@@ -3,7 +3,7 @@ import re
 import praw
 import OAuth2Util
 import sqlite3
-
+from time import sleep
 
 class UpdatesBot:
 
@@ -19,6 +19,10 @@ class UpdatesBot:
                            (ID INTEGER PRIMARY KEY AUTOINCREMENT,
                            BRANCH TEXT NOT NULL UNIQUE,
                            VERSION TEXT NOT NULL)''')
+
+        self.db.execute('''CREATE TABLE IF NOT EXISTS SUBSCRIBERS
+            (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            USER TEXT NOT NULL UNIQUE)''')
 
         self.db.commit()
 
@@ -52,7 +56,7 @@ class UpdatesBot:
 
         result = re.findall(r'\d\.\d\.\d\.\d{5}\.\d{2}', str(data.read()), flags=0)
 
-        self.new_stable = "asd"
+        self.new_stable = result[0]
         self.new_open_beta = result[1]
         self.new_open_alpha = result[2]
 
@@ -78,7 +82,6 @@ class UpdatesBot:
             self.cur.execute('''UPDATE CURVERSION SET VERSION = ? WHERE ID = 3;''', (result[2],))
 
         self.db.commit()
-        self.db.close()
 
         if len(changes) > 0:
             self.send_messages(changes)
@@ -89,8 +92,57 @@ class UpdatesBot:
 
         self.r.send_message("santi871", "hello", message)
 
+    def watch_thread(self, thread_id):
+
+        submission = self.r.get_submission(submission_id=thread_id)
+        already_done = []
+
+        while True:
+
+            submission.replace_more_comments(limit=None, threshold=0)
+            all_comments = praw.helpers.flatten_tree(submission.comments)
+
+            for comment in all_comments:
+                if comment.body == "subscribe" and comment.permalink not in already_done:
+
+                    try:
+                        self.cur.execute('''INSERT INTO SUBSCRIBERS(USER) VALUES(?)''', (str(comment.author),))
+                        self.db.commit()
+                    except Exception:
+                        pass
+
+                    already_done.append(comment.permalink)
+
+            sleep(20)
+
+    def watch_messages(self):
+
+        messages = self.r.get_messages()
+        already_done = []
+
+        while True:
+
+            for message in messages:
+
+                if message.body == "unsubscribe me" and message.id not in already_done:
+
+                    try:
+                        self.cur.execute('''DELETE FROM SUBSCRIBERS WHERE USER = ?;''', (str(message.author),))
+                        self.db.commit()
+                    except Exception:
+                        pass
+
+                    already_done.append(message.id)
+
+            sleep(30)
+
+    def closedb(self):
+
+        self.db.close()
+
 bot = UpdatesBot()
-bot.check_website()
+bot.watch_messages()
+bot.closedb()
 
 
 
