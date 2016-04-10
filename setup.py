3e5626a1bff60492551241ffb8acc9da38b37606
Santi871 <santivegega@gmail.com>
2016-04-10 18:42:30 -0300

    added setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from cx_Freeze import setup, Executable
 
 setup(name = "DCS Updates Bot" ,
-      version = "0.1" ,
+      version = "0.2" ,
       description = "" ,
       executables = [Executable("main.py")])
